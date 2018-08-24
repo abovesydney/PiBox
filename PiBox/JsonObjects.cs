1f5c67eb08f53a0e905105c7b56c6395c1060c0c
AboveSydney <35410850+AboveSydney@users.noreply.github.com>
2018-08-24 14:45:18 +1000

    Basic flightlist page, images, HTTP error handling.

---
+++ PiBox/JsonObjects.cs
@@ -0,0 +1,116 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using PiBox;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using System.Collections.ObjectModel;
+
+namespace PiBox
+{
+
+    public class Feed
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public bool polarPlot { get; set; }
+    }
+
+    public class AcList
+    {
+        public int Id { get; set; }
+        public int Rcvr { get; set; }
+        public bool HasSig { get; set; }
+        public string Icao { get; set; }
+        public bool Bad { get; set; }
+        public string Reg { get; set; }
+        public DateTime FSeen { get; set; }
+        public int TSecs { get; set; }
+        public int CMsgs { get; set; }
+        public int Alt { get; set; }
+        public int GAlt { get; set; }
+        public int AltT { get; set; }
+        public bool Tisb { get; set; }
+        public float Spd { get; set; }
+        public double Trak { get; set; }
+        public bool TrkH { get; set; }
+        public string Type { get; set; }
+        public string Mdl { get; set; }
+        public string Man { get; set; }
+        public string CNum { get; set; }
+        public bool IsFerryFlight { get; set; }
+        public bool IsCharterFlight { get; set; }
+        public string Op { get; set; }
+        public string OpIcao { get; set; }
+        public string Sqk { get; set; }
+        public bool Ident { get; set; }
+        public bool Help { get; set; }
+        public int VsiT { get; set; }
+        public int WTC { get; set; }
+        public int Species { get; set; }
+        public string Engines { get; set; }
+        public int EngType { get; set; }
+        public int EngMount { get; set; }
+        public bool Mil { get; set; }
+        public string Cou { get; set; }
+        public bool HasPic { get; set; }
+        public bool Interested { get; set; }
+        public int FlightsCount { get; set; }
+        public bool Gnd { get; set; }
+        public int SpdTyp { get; set; }
+        public bool CallSus { get; set; }
+        public int Trt { get; set; }
+        public string Year { get; set; }
+        public int? Vsi { get; set; }
+        public double? InHg { get; set; }
+        public string Call { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long? PosTime { get; set; }
+        public bool? Mlat { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public List<string> Stops { get; set; }
+        public bool? PosStale { get; set; }
+        public string FullUri
+        {
+            get
+            {
+                return "/Assets/logos/" + OpIcao + ".png";
+            }
+        }
+    }
+
+    public class RootObject
+    {
+        public int src { get; set; }
+        public List<Feed> feeds { get; set; }
+        public int srcFeed { get; set; }
+        public bool showSil { get; set; }
+        public bool showFlg { get; set; }
+        public bool showPic { get; set; }
+        public int flgH { get; set; }
+        public int flgW { get; set; }
+        public List<AcList> acList { get; set; }
+        public int totalAc { get; set; }
+        public string lastDv { get; set; }
+        public int shtTrlSec { get; set; }
+        public long stm { get; set; }
+        public bool configChanged { get; set; }
+        public int status { get; set; }
+        public int? count { get; set; }
+        public List<Datum> data { get; set; }
+        public string error { get; set; }
+
+        public class Datum
+        {
+            public string image { get; set; }
+            public string link { get; set; }
+            public string photographer { get; set; }
+        }
+    }
+}
--- PiBox/MainPage.xaml.cs
+++ PiBox/MainPage.xaml.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -24,7 +29,70 @@ namespace PiBox
     {
         public MainPage()
         {
+            GC.Collect(2);
+            DoTheMain();
             this.InitializeComponent();
+        }
+
+        public async void DoTheMain()
+        {
+
+            var jsonSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Include,
+                MissingMemberHandling = MissingMemberHandling.Ignore
+            };
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    var response = await client.GetAsync("http://www.abovesydney.net:8080/VirtualRadar/aircraftlist.json");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //Decode JSON to list here
+                        var jsonString = await response.Content.ReadAsStringAsync();
+                        RootObject ro = JsonConvert.DeserializeObject<RootObject>(jsonString);
+
+                        RootObject rootObject = new RootObject();
+                        AcList acList = new AcList();
+                        FlightList.ItemsSource = ro.acList;
+                        
+                    }
+                    else
+                    {
+                        Debug.WriteLine("ERROR");
+                        OnHTTPFail();
+                    }
+                }
+                finally
+                {
+                    client.Dispose();
+                }
+
+                async void OnHTTPFail()
+                {
+                    var messageDialog = new MessageDialog("Error getting JSON file.");
+                    await messageDialog.ShowAsync();
+                }
+
+            }
+        }
+
+        private void _DoRetry(object sender, TappedRoutedEventArgs e)
+        {
+            DoTheMain();
+        }
+
+        private void _pickFlight(object sender, SelectionChangedEventArgs e)
+        {
+            var _SelectedLine = (AcList)FlightList.SelectedItem;
+            var _SelectedFlight = _SelectedLine.Icao.ToString();
+            Debug.WriteLine(_SelectedFlight);
+            Frame.Navigate(typeof(Pages.FlightRecord), _SelectedFlight, new DrillInNavigationTransitionInfo());
         }
     }
 }
